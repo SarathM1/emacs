89a5480c95d4f8ff9ce188fb7fd7a48ffce9aa34
Personal Laptop <sarathm00@gmail.com>
2017-07-01 23:46:01 +0530

    First commit

---
+++ init.el
@@ -0,0 +1,71 @@
+;; @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+;;    Installing use-package
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+;; @@@@@@@@@@@@@@@@@@@@@@@@@@@
+;; Add all lines after this!!!
+
+;; Evil-mode
+;; Refer: [https://blog.aaronbieber.com/2016/01/23/living-in-evil.html]
+
+;; Set defualt font
+;;(set-frame-font "Inconsolata 17" nil t)
+(set-face-attribute 'default nil
+		    :family "Source Code Pro"
+		    :height 160
+		    :weight 'normal
+		    :width 'normal)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  ;; More configuration goes here
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode))
+
+  ;(use-package evil-jumper
+   ; :ensure t
+    ;:config
+    ;(global-evil-jumper-mode))
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+    :ensure t)
+)
+
+
+(load-theme 'monokai t)
+
+(global-linum-mode t)
+(setq linum-format "%4d ")
+(set-face-attribute 'fringe nil :background "black")
+
+;; make both fringes 4 pixels wide
+(fringe-mode 4)
